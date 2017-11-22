cf90cacbb5bb59b5be8ffaaefde5eb5bb0e38c8c
bangks72 <33773553+bangks72@users.noreply.github.com>
2017-11-22 20:18:01 +0900

    [gitTest-003]ks 호출
    호출

--- src/com/test/kyungwon/GitTest.java
+++ src/com/test/kyungwon/GitTest.java
@@ -5,5 +5,9 @@ public class GitTest {
 	public static void main(String[] args) {
 		System.out.println("깃허브 테스트");
 		System.out.println("UTF-8 설정");
+		
+		KyeongSik ks  = new KyeongSik();
+		
+		ks.ks();
 	}
 }
